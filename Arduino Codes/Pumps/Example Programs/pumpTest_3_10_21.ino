bf46fb1f087ec454b9bb9d642cce230b56d644a2
dman191 <66878782+dman191@users.noreply.github.com>
2021-03-11 13:10:09 -0500

    Add files via upload
    pumpTest.ino modified to perform sequence test of pumps 6-11 with 2 seconds for each pump to be turned on at a time.

---
+++ Arduino Codes/Pumps/Example Programs/pumpTest_3_10_21.ino
@@ -0,0 +1,36 @@
+
+
+//Number of pumps with pins from NUM_PUMPS-1 to NUM_PUMPS (Not inclusive)
+  //If NUM_PUMPS == 2:   pumps on pins 0 & 1
+#define NUM_PUMPS 12 
+
+void setup() {
+
+  for(int ii =0; ii<NUM_PUMPS;ii++){
+    pinMode(ii,OUTPUT);
+  }
+  
+
+}
+
+void loop() {
+//adjusted to run in sequence for 2 seconds on per pump
+for (int jj=6; jj<NUM_PUMPS;jj++){ //to test from pumps 6-11
+  digitalWrite(jj,HIGH);
+  delay(2000);
+  digitalWrite(jj,LOW);
+}
+/*  for(int jj=0; jj<NUM_PUMPS;jj++){
+    digitalWrite(jj,HIGH);
+  }
+*/
+/* //Uncomment if you want pumps to alternate between on and off
+  delay(1000); //Delay in microseconds
+  for(int jj=0; jj<NUM_PUMPS;jj++){
+    digitalWrite(jj,LOW);
+  }
+  delay(1000);
+*/
+  
+
+}
